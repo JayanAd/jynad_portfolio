80502abd08b8764087fe29d9dede5ea8f1e4c71a
Jayan Adhikari <adhikarijayan1@gmail.com>
2025-10-13 13:21:27 +0545

    path resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+// https://vitejs.dev/config/
+export default defineConfig({ 
+  plugins: [react(), tsconfigPaths()],
+  // base: "/",
+  server:{
+    open: true,
+    port:3030
+  },
+});
